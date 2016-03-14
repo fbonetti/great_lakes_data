a46cff38cf46299fc20e25d082014a86301d2326
Frank Bonetti <frank.r.bonetti@gmail.com>
2016-03-13 21:32:27 -0500

    remove area styling

--- app/assets/javascripts/readings.js
+++ app/assets/javascripts/readings.js
@@ -49,32 +49,7 @@ var initDailyAverageChart = function() {
     legend: {
       enabled: false
     },
-    plotOptions: {
-      area: {
-        fillColor: {
-          linearGradient: {
-            x1: 0,
-            y1: 0,
-            x2: 0,
-            y2: 1
-          },
-          stops: [
-            [0, Highcharts.getOptions().colors[0]],
-            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-          ]
-        },
-        lineWidth: 1,
-        states: {
-          hover: {
-            lineWidth: 1
-          }
-        },
-        threshold: null
-      }
-    },
-
     series: [{
-      type: 'area',
       name: 'Wind speed',
       data: []
     }]
